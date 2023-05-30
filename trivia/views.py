334de1ee1c38dded446647535c197df98377dde8
CamilaPaiz <camilapaiz@hotmail.com>
2023-05-30 19:12:09 -0300

    feat:add QuestionViewSet

--- trivia/views.py
+++ trivia/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Question
+from .serializers import QuestionSerializer
 
-# Create your views here.
+
+class QuestionViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
